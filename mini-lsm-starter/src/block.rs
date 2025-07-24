f0a62d1309bedd31e19fc336dd7198ecda52caa2
reolsaikou <reolsaikou@gmail.com>
2025-07-25 00:53:52 +0800

    week 1 day 3 task 1 compeleted

--- mini-lsm-starter/src/block.rs
+++ mini-lsm-starter/src/block.rs
@@ -32,11 +32,46 @@ impl Block {
     /// Encode the internal data to the data layout illustrated in the course
     /// Note: You may want to recheck if any of the expected field is missing from your output
     pub fn encode(&self) -> Bytes {
-        unimplemented!()
+        let mut ans = Vec::new();
+        ans.extend_from_slice(&self.data[..]);
+        let ptr = self.offsets.as_ptr().cast::<u8>();
+        ans.extend_from_slice(unsafe { std::slice::from_raw_parts(ptr, self.offsets.len() * 2) });
+        Bytes::from(ans)
     }
 
     /// Decode from the data layout, transform the input `data` to a single `Block`
     pub fn decode(data: &[u8]) -> Self {
-        unimplemented!()
+        let data_vec: Vec<u8>;
+        let mut block = Self {
+            data: [].to_vec(),
+            offsets: [].to_vec(),
+        };
+        if data.is_empty() {
+            block
+        } else {
+            let cnt: usize = u16::from_ne_bytes(*data.last_chunk::<2>().unwrap()) as usize;
+            // println!("cnt {}", cnt);
+            // cnt = (cnt << 8) + data[data.len() - 1] as usize;
+            // println!("data len {}, cnt {}", data.len(), cnt);
+            let upper_bound = data.len() - cnt * 2 - 2;
+            // println!("upper bound {}", upper_bound);
+            block.data.extend_from_slice(&data[..upper_bound]);
+            // let ptr = data.as_ptr();
+            // unsafe {
+            //     let aling_size = std::mem::align_of::<u8>();
+            //     println!("align_size {}",aling_size);
+            //     println!("data {}", ptr.add(upper_bound) as usize);
+            //     println!("ans {}", ptr.add(upper_bound) as usize % aling_size);
+            //     println!("Value: ptr.add(upper_bound) {}", *ptr.add(upper_bound+1));
+            // }
+            let tail = &data[upper_bound..];
+            let offsets: Vec<u16> = tail
+                .chunks_exact(2)
+                .take(cnt)
+                .map(|s| u16::from_ne_bytes([s[0], s[1]]))
+                .collect();
+            block.offsets.extend_from_slice(&offsets);
+            block
+        }
     }
 }
--- mini-lsm-starter/src/block/builder.rs
+++ mini-lsm-starter/src/block/builder.rs
@@ -15,9 +15,10 @@
 #![allow(unused_variables)] // TODO(you): remove this lint after implementing this mod
 #![allow(dead_code)] // TODO(you): remove this lint after implementing this mod
 
-use crate::key::{KeySlice, KeyVec};
+use bytes::BufMut;
 
 use super::Block;
+use crate::key::{KeySlice, KeyVec};
 
 /// Builds a block.
 pub struct BlockBuilder {
@@ -34,22 +35,55 @@ pub struct BlockBuilder {
 impl BlockBuilder {
     /// Creates a new block builder.
     pub fn new(block_size: usize) -> Self {
-        unimplemented!()
+        Self {
+            offsets: Vec::new(),
+            data: Vec::new(),
+            block_size,
+            first_key: KeyVec::new(),
+        }
     }
 
     /// Adds a key-value pair to the block. Returns false when the block is full.
     #[must_use]
     pub fn add(&mut self, key: KeySlice, value: &[u8]) -> bool {
-        unimplemented!()
+        let mut size = self.offsets.len() + self.data.len() + 2;
+        size += 6 + key.len() + value.len();
+
+        if size >= self.block_size && !self.is_empty() {
+            return false;
+        }
+
+        if self.is_empty() {
+            self.first_key = key.to_key_vec();
+        }
+
+        let key_len = key.len() as u16;
+        let val_len = value.len() as u16;
+        self.offsets.push(self.data.len() as u16);
+        self.data.extend_from_slice(&key_len.to_ne_bytes());
+        self.data.extend_from_slice(key.into_inner());
+        self.data.extend_from_slice(&val_len.to_ne_bytes());
+        self.data.extend_from_slice(value);
+        true
     }
 
     /// Check if there is no key-value pair in the block.
     pub fn is_empty(&self) -> bool {
-        unimplemented!()
+        self.offsets.is_empty()
     }
 
     /// Finalize the block.
     pub fn build(self) -> Block {
-        unimplemented!()
+        let mut data = Vec::new();
+        data.extend_from_slice(&self.data);
+        data.extend_from_slice(
+            &self
+                .offsets
+                .iter()
+                .flat_map(|&v| v.to_ne_bytes())
+                .collect::<Vec<u8>>(),
+        );
+        data.put_u16_ne(self.offsets.len() as u16);
+        Block::decode(&data)
     }
 }
